a01f9bcc7fdb35476562ca5e2e9588d620ee381f
mohamed <engmido1002@gmail.com>
2025-08-03 22:26:44 +0300

    Add App Fonts

---
+++ lib/core/utils/app_fonts.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+abstract class AppFonts {
+
+  static const textStyle20=TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+  );
+  static const textStyle16=TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+  );
+  
+}

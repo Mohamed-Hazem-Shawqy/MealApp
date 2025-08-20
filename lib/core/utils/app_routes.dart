291c1c9dd3db25c47cc40584e2f989c5458585e7
mohamed <engmido1002@gmail.com>
2025-08-20 14:37:06 +0300

    app route modifiy

--- lib/core/utils/app_routes.dart
+++ lib/core/utils/app_routes.dart
@@ -2,18 +2,23 @@ import 'package:go_router/go_router.dart';
 import 'package:meal_app/features/auth/presentation/view/login.dart';
 import 'package:meal_app/features/auth/presentation/view/signup.dart';
 import 'package:meal_app/features/home/presentation/view/home_screen.dart';
-import 'package:meal_app/features/home/presentation/view/profile_screen.dart';
+import 'package:meal_app/features/onboarding/onboarding_screen_two.dart';
+import 'package:meal_app/features/onboarding/onboarding_screenone.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract class AppRoutes {
   static const kLogin = '/';
   static const kHome = '/home';
   static const kSignup = '/signup';
+  static const kOnboardingScreenOne = '/OnboardingScreenOne';
+  static const kOnboardingScreenTwo = '/OnboardingScreenTwo';
 
   static final router = GoRouter(
     redirect: (context, state) {
       final bool loginRoute = state.uri.path == kLogin;
       final bool signupRoute = state.uri.path == kSignup;
+      final bool onBordaingOneRoute = state.uri.path == kOnboardingScreenOne;
+      final bool onBoardingSTwoRoute = state.uri.path == kOnboardingScreenTwo;
 
       final bool homeRoute = state.uri.path == kHome;
       final bool isLoggedIn =
@@ -21,7 +26,7 @@ abstract class AppRoutes {
 
       if (!isLoggedIn) {
         print('========================notlogin================');
-        if (loginRoute||signupRoute) {
+        if (loginRoute||signupRoute||onBordaingOneRoute||onBoardingSTwoRoute) {
           return null;
         }
         return kLogin;
@@ -40,6 +45,8 @@ abstract class AppRoutes {
       GoRoute(path: kLogin, builder: (context, state) => const Login()),
       GoRoute(path: kSignup, builder: (context, state) => const Signup()),
       GoRoute(path: kHome, builder: (context, state) => HomeScreen()),
+      GoRoute(path: kOnboardingScreenOne, builder: (context, state) => OnboardingScreenOne()),
+      GoRoute(path: kOnboardingScreenTwo, builder: (context, state) => OnboardingScreenTwo()),
     ],
   );
 }

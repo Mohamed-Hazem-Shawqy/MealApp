833de85b05c7bb7f5c8f992ec2ca0ddec7613649
Yousef Abdel Samad <yousef.ahmed.0402@gmail.com>
2025-08-21 22:50:46 +0300

    delete package flutter_gemini and use google_generative_ai

--- lib/features/gemini/data/repo_impl.dart
+++ lib/features/gemini/data/repo_impl.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:meal_app/features/gemini/domain/repo_decl.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
 
 class RepoImpl implements RepoDecl {
+  final GenerativeModel _model;
+
+  RepoImpl()
+    : _model = GenerativeModel(
+        model: "gemini-2.0-flash",
+        apiKey: "AIzaSyAbF7I-cHkU_EBXZlv23P29pFHwm0yEhfQ",
+      );
+  // Gemini.init(apiKey:"AIzaSyAbF7I-cHkU_EBXZlv23P29pFHwm0yEhfQ");
+  //Gemini.init(apiKey:"AIzaSyCY5WBbA4EDbG-YQ-W5kW8hisEUmSBJNbA");
+  // AIzaSyCY5WBbA4EDbG-YQ-W5kW8hisEUmSBJNbA   (cloud)
+  //AIzaSyCE_2j-vGUDY7ReUoIvynwsLJeor3T6_SU    (Ai Studio)
+
   @override
   Future<String> geminiChat(String text) async {
-    final gemini = Gemini.instance;
-
-    final response = await gemini.chat([
-      Content(parts: [Part.text(text)], role: 'user'),
-      Content(parts: [Part.text(text)], role: 'model'),
-    ]);
-    return response?.output ?? "No response";
+    final prompt = '''
+Hi, my name is Yousef.:
+''';
+    final content = [Content.text(prompt)];
+    final chat = _model.startChat(history: content);
+    final response = await chat.sendMessage(Content.text(text));
+    return response.text ?? 'Error: No response';
   }
 
   @override

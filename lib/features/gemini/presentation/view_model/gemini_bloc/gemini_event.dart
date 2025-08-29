e3850286beaa649d3bb0fa51eeebe19b9e9dcc8c
Mahmoud Nasser <126198892+MahmoudNasser00@users.noreply.github.com>
2025-08-29 17:51:10 +0300

    update files

---
+++ lib/features/gemini/presentation/view_model/gemini_bloc/gemini_event.dart
@@ -0,0 +1,4 @@
+part of 'gemini_bloc.dart';
+
+@immutable
+sealed class GeminiEvent {}
---
+++ lib/features/gemini/presentation/view_model/gemini_bloc/gemini_state.dart
@@ -0,0 +1,6 @@
+part of 'gemini_bloc.dart';
+
+@immutable
+sealed class GeminiState {}
+
+final class GeminiInitial extends GeminiState {}
